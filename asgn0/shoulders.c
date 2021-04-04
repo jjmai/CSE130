51250271edd3e181d23bfe98e6872cc58e248c82
Jordan Mai <jmai12@unix3.lt.ucsc.edu>
2021-04-04 15:48:10 -0700

    examples are correct

--- asgn0/shoulders.c
+++ asgn0/shoulders.c
@@ -10,17 +10,27 @@ int main(int argc, char *argv[]) {
   int length = atoi(argv[1]);
   char buffer[length];
   int n = 0;
-
-  for (int i = 2; i < argc; i++) {
-    if (strcmp(argv[i], "-") == 0) {
-      n = read(STDIN_FILENO, buffer, length);
-    } else {
-
-      infile = open(argv[i], O_RDONLY | O_CREAT);
-      n = read(infile, buffer, length);
-      buffer[n] = '\0';
-    }
+  // Base Case for no arguments input
+  if (argc == 2) {
+    n = read(infile, buffer, length);
+    buffer[n] = '\0';
     write(outfile, buffer, n);
+  } else {
+
+    for (int i = 2; i < argc; i++) {
+      if (strcmp(argv[i], "-") == 0) {
+        n = read(STDIN_FILENO, buffer, length);
+        if (n == -1)
+          perror("FILE EMPTY");
+      } else {
+
+        infile = open(argv[i], O_RDONLY | O_CREAT);
+        n = read(infile, buffer, length);
+        buffer[n] = '\0';
+      }
+      write(outfile, buffer, n);
+      close(infile);
+    } 
   }
   printf("\n");
   close(infile);
