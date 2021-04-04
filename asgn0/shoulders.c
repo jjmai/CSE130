eb13f638161ce4c351d05c1d97390f3c4f0f5bc4
Jordan Mai <jmai12@unix1.lt.ucsc.edu>
2021-04-03 18:32:06 -0700

    can read

--- asgn0/shoulders.c
+++ asgn0/shoulders.c
@@ -2,6 +2,7 @@
 #include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 int main(int argc, char *argv[]) {
   int infile = STDIN_FILENO;
@@ -11,12 +12,14 @@ int main(int argc, char *argv[]) {
   int n = 0;
 
   for (int i = 2; i < argc; i++) {
+    if (strcmp(argv[i], "-") == 0) {
+      n = read(STDIN_FILENO, buffer, length);
+    } else {
 
-    infile = open(argv[i], O_RDONLY | O_CREAT);
-
-    n = read(infile, buffer, length);
-
-    buffer[n] = '\0';
+      infile = open(argv[i], O_RDONLY | O_CREAT);
+      n = read(infile, buffer, length);
+      buffer[n] = '\0';
+    }
     write(outfile, buffer, n);
   }
   printf("\n");
